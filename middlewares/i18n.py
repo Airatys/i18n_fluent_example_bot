638d3d4b3156fd5ed60251bd094ea45bc6c8a49d
Airat Sharifullin <sharifullin.airat@yandex.ru>
2025-12-13 17:56:56 +0300

    add locales

--- middlewares/i18n.py
+++ middlewares/i18n.py
@@ -1,25 +1,27 @@
-from fluent_compiler.bundle import FluentBundle
+import logging
+from typing import Any, Awaitable, Callable, Dict
 
-from fluentogram import FluentTranslator, TranslatorHub
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject, User
+from fluentogram import TranslatorHub
+
+logger = logging.getLogger(__name__)
 
 
-def create_translator_hub() -> TranslatorHub:
-    translator_hub = TranslatorHub(
-        {
-            "ru": ("ru", "en"),
-            "en": ("en", "ru")
-        },
-        [
-            FluentTranslator(
-                locale="ru",
-                translator=FluentBundle.from_files(
-                    locale="ru-RU",
-                    filenames=["locales/ru/LC_MESSAGES/txt.ftl"])),
-            FluentTranslator(
-                locale="en",
-                translator=FluentBundle.from_files(
-                    locale="en-US",
-                    filenames=["locales/en/LC_MESSAGES/txt.ftl"]))
-        ],
-    )
-    return translator_hub
+class TranslatorRunnerMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any]
+    ) -> Any:
+
+        user: User = data.get('event_from_user')
+
+        if user is None:
+            return await handler(event, data)
+
+        hub: TranslatorHub = data.get('_translator_hub')
+        data['i18n'] = hub.get_translator_by_locale(locale=user.language_code)
+
+        return await handler(event, data)
